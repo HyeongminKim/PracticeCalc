91f1e7028ad0245c5cdc52dd3f9c30c91b715b38
HyeongminKim <assa0620@gmail.com>
2021-08-10 21:53:03 +0900

    분석 기능이 한 문제도 풀지 않았을 경우 0을 표시 못하는 문제 수정

--- PracticeCalc/Assets/Scripts/Home/AnalysisController.cs
+++ PracticeCalc/Assets/Scripts/Home/AnalysisController.cs
@@ -4,7 +4,11 @@ using UnityEngine.UI;
 namespace Home {
     public class AnalysisController : MonoBehaviour {
         private void Start() {
-            gameObject.GetComponent<Text>().text = $"총 {GameManager.instance.triedQuestion:##,###}문항을 시도하였으며\n그 중 {GameManager.instance.performance:F2}%가 정답입니다. ";
+            if(GameManager.instance.triedQuestion == 0) {
+                gameObject.GetComponent<Text>().text = "암산 연습 앱에 오신 것을 환영합니다!\n지금 바로 암산 연습을 시작해 보세요";
+            } else {
+                gameObject.GetComponent<Text>().text = $"총 {GameManager.instance.triedQuestion:##,###}문항을 시도하였으며\n그 중 {GameManager.instance.performance:F2}%가 정답입니다. ";
+            }
         }
     }
 }
